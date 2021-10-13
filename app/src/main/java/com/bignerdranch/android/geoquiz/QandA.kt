44b71b55c0eda5d972bf45036c1cfcebae45f85d
HP <Abdulaziz.Alhothali.jed.k1@tuwaiq.edu.sa>
2021-10-13 21:41:57 +0300

    HomeWork!!!

---
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QandA.kt
@@ -0,0 +1,11 @@
+package com.bignerdranch.android.geoquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QandA : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_qand)
+    }
+}
